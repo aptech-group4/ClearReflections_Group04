78b07d12509d8261b37554601d1777bbb4432968
aptech-group4 <105347078+aptech-group4@users.noreply.github.com>
2022-05-17 15:21:03 +0700

    Add files via upload

---
+++ source_code/script.js
@@ -0,0 +1,304 @@
+let products = {
+    data: [
+      {
+        productName: "Large Italian Giltwood Antique Mirror",
+        category: "Drawing/LivingRoomMirrors",
+        price: "5000",
+        materials: "Woodenframed",
+        productBrand: "Viglacera",
+        shape: "Rectangle",
+        image: "Image/Mirrors 1.png",
+        file: "Mirrors/Mirror 1.docx",
+      },
+      {
+        productName: "Red/Gold Wide Floral Mirror",
+        category: "Drawing/LivingRoomMirrors",
+        price: "3500",
+        materials: "Woodenframed",
+        productBrand: "Viglacera",
+        shape: "Rectangle",
+        image: "Image/Mirrors 2.png",
+        file: "Mirrors/Mirrors 2.docx",
+      },
+      {
+        productName: "Custom Made High Gloss Chinoiserie Brass Ming Hardware Wall Mirror",
+        category: "Drawing/LivingRoomMirrors",
+        price: "450",
+        materials: "Woodenframed",
+        productBrand: "Viglacera",
+        shape: "Rectangle",
+        image: "Image/Mirrors 3.png",
+        file: "Mirrors/Mirrors 3.docx",
+      },
+      {
+        productName: "Antique 19th Century French Renaissance Revival Over Mantle Mirror",
+        category: "Drawing/LivingRoomMirrors",
+        price: "6000",
+        materials: "AcrylicBordered",
+        productBrand: "Viglacera",
+        shape: "Rectangle",
+        image: "Image/Mirrors 4.png",
+        file: "Mirrors/Mirrors 4.docx",
+      },
+      {
+        productName: "Regency Style Gold Decorated Beveled Glass Mirror",
+        category: "Drawing/LivingRoomMirrors",
+        price: "500",
+        materials: "AcrylicBordered",
+        productBrand: "Viglacera",
+        shape: "Rectangle",
+        image: "Image/Mirrors 5.png",
+        file: "Mirrors/Mirrors 5.docx",
+      },
+      {
+        productName: "Vintage Large Carved Giltwood & Gesso Italian Wall Mirror",
+        category: "BathroomMirrors",
+        price: "1285",
+        materials: "AcrylicBordered",
+        productBrand: "Viglacera",
+        shape: "Rectangle",
+        image: "Image/Mirrors 6.png",
+        file: "Mirrors/Mirrors 6.docx",
+      },
+      {
+        productName: "Antique Art Nouveau Brass Table Mirror or Picture Frame",
+        category: "BathroomMirrors",
+        price: "567",
+        materials: "AcrylicBordered",
+        productBrand: "Viglacera",
+        shape: "Rectangle",
+        image: "Image/Mirrors 7.png",
+        file: "Mirrors/Mirrors 7.docx",
+      },
+      {
+        productName: "1970s Mid-Century Modern Chinoiserie La Barge Painted Rectangular Wall Mirror",
+        category: "BathroomMirrors",
+        price: "1500",
+        materials: "MetalBordered",
+        productBrand: "Viglacera",
+        shape: "Rectangle",
+        image: "Image/Mirrors 8.png",
+        file: "Mirrors/Mirrors 8.docx",
+      },
+      {
+        productName: "1950s Venetian Scalloped Gilt Mirror",
+        category: "BathroomMirrors",
+        price: "4000",
+        materials: "MetalBordered",
+        productBrand: "Viglacera",
+        shape: "Rectangle",
+        image: "Image/Mirrors 9.png",
+        file: "Mirrors/Mirrors 9.docx",
+      },
+      {
+        productName: "Celerie Kemble for Mirror Home Dark Mahogany Mirror",
+        category: "BathroomMirrors",
+        price: "2518",
+        materials: "MetalBordered",
+        productBrand: "Viglacera",
+        shape: "Rectangle",
+        image: "Image/Mirrors 10.png",
+        file: "Mirrors/Mirrors 10.docx",
+      },
+      {
+        productName: "Century Furniture Grand Convex Mirror",
+        category: "DressingMirrors",
+        price: "5200",
+        materials: "DecorativeMirrors",
+        productBrand: "Inax",
+        shape: "Circle",
+        image: "Image/Mirrors 11.png",
+        file: "Mirrors/Mirrors 11.docx",
+      },
+      {
+        productName: "Bernhardt Loft Piper Round Mirror in Brushed White",
+        category: "DressingMirrors",
+        price: "470",
+        materials: "DecorativeMirrors",
+        productBrand: "Inax",
+        shape: "Circle",
+        image: "Image/Mirrors 12.png",
+        file: "Mirrors/Mirrors 12.docx",
+      },
+      {
+        productName: "Porthole Mirror in Antique Gold",
+        category: "DressingMirrors",
+        price: "265",
+        materials: "DecorativeMirrors",
+        productBrand: "Inax",
+        shape: "Circle",
+        image: "Image/Mirrors 13.png",
+        file: "Mirrors/Mirrors 13.docx",
+      },
+      {
+        productName: "Vintage Hand Hammered Round Copper Wall Mirror",
+        category: "DressingMirrors",
+        price: "385",
+        materials: "DecorativeMirrors",
+        productBrand: "Inax",
+        shape: "Circle",
+        image: "Image/Mirrors 14.png",
+        file: "Mirrors/Mirrors 14.docx",
+      },
+      {
+        productName: "Mid-Century Shell Frame Round Mirror",
+        category: "BedroomMirrors",
+        price: "475",
+        materials: "LEDMirrors",
+        productBrand: "Inax",
+        shape: "Circle",
+        image: "Image/Mirrors 15.png",
+        file: "Mirrors/Mirrors 15.docx",
+      },
+      {
+        productName: "1920s French Two-Tiered Sunburst Mirror",
+        category: "BedroomMirrors",
+        price: "3495",
+        materials: "LEDMirrors",
+        productBrand: "Citybuilding",
+        shape: "Circle",
+        image: "Image/Mirrors 16.png",
+        file: "Mirrors/Mirrors 16.docx",
+      },
+      {
+        productName: "1990s Grand Baroque Mirror",
+        category: "BedroomMirrors",
+        price: "785",
+        materials: "LEDMirrors",
+        productBrand: "Citybuilding",
+        shape: "Circle",
+        image: "Image/Mirrors 17.png",
+        file: "Mirrors/Mirrors 17.docx",
+      },
+      {
+        productName: "Quartz Crystal Encrusted Circular Mirror by Marjorie Skouras, White, 42",
+        category: "BedroomMirrors",
+        price: "3785",
+        materials: "BluetoothMirrors",
+        productBrand: "Citybuilding",
+        shape: "Circle",
+        image: "Image/Mirrors 18.png",
+        file: "Mirrors/Mirrors 18.docx",
+      },
+      {
+        productName: "Fleur Home x Chairish Flambeau Circle Mirror in Gold Leaf, 48x48",
+        category: "BedroomMirrors",
+        price: "3640",
+        materials: "BluetoothMirrors",
+        productBrand: "Citybuilding",
+        shape: "Circle",
+        image: "Image/Mirrors 19.png",
+        file: "Mirrors/Mirrors 19.docx",
+      },
+      {
+        productName: "Fleur Home x Chairish Audobon Peony Circle Mirror in Parma Gray, 48x48",
+        category: "BedroomMirrors",
+        price: "2990",
+        materials: "BluetoothMirrors",
+        productBrand: "Citybuilding",
+        shape: "Circle",
+        image: "Image/Mirrors 20.png",
+        file: "Mirrors/Mirrors 20.docx",
+      },
+    ],
+  };
+  
+  for (let i of products.data) {
+    //Create Card
+    let card = document.createElement("div");
+    //Card should have category and should stay hidden initially
+    card.classList.add("card", i.category, "hide");
+    //Card should have materials and should stay hidden initially
+    card.classList.add("card", i.materials, "hide");
+    //Card should have shape and should stay hidden initially
+    card.classList.add("card", i.shape, "hide");
+    //image div
+    let imgContainer = document.createElement("div");
+    imgContainer.classList.add("image-container");
+    //img tag
+    let image = document.createElement("img");
+    image.setAttribute("src", i.image);
+    imgContainer.appendChild(image);
+    card.appendChild(imgContainer);
+    //container
+    let container = document.createElement("div");
+    container.classList.add("container");
+    //product name
+    let name = document.createElement("h4");
+    name.classList.add("product-name");
+    name.innerText = i.productName.toUpperCase();
+    container.appendChild(name);
+    //brand
+    let brand = document.createElement("h5");
+    brand.classList.add("product-brand");
+    brand.innerText = "Brand: " + i.productBrand.toUpperCase();
+    card.classList.add("card", i.productBrand);
+    container.appendChild(brand);
+    //price
+    let price = document.createElement("h6");
+    price.innerText = "$" + i.price;
+    container.appendChild(price);
+    //link download
+    let downloadLink = document.createElement("a");
+    downloadLink.innerText = "Download";
+    downloadLink.href = i.file;
+    container.appendChild(downloadLink);
+    
+    card.appendChild(container);
+    document.getElementById("products").appendChild(card);
+  }
+  
+  //parameter passed from button (Parameter same as category)
+  function filterProduct(value) {
+    //Button class code
+    let buttons = document.querySelectorAll(".button-value");
+    buttons.forEach((button) => {
+      //check if value equals innerText
+      if (value.toUpperCase() == button.innerText.toUpperCase()) {
+        button.classList.add("active");
+      } else {
+        button.classList.remove("active");
+      }
+    });
+  
+    //select all cards
+    let elements = document.querySelectorAll(".card");
+    //loop through all cards
+    elements.forEach((element) => {
+      //display all cards on 'all' button click
+      if (value == "all") {
+        element.classList.remove("hide");
+      } else {
+        //Check if element contains category class
+        if (element.classList.contains(value)) {
+          //display element based on category
+          element.classList.remove("hide");
+        } else {
+          //hide other elements
+          element.classList.add("hide");
+        }
+      }
+    });
+  }
+  
+  
+  
+  
+  //Initially display all products
+  window.onload = () => {
+    filterProduct("all");
+  };
+
+  //onscroll
+window.onscroll = function() {myFunction()};
+
+var menu = document.getElementById("menu");
+var sticky = menu.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    menu.classList.add("sticky")
+  } else {
+    menu.classList.remove("sticky");
+  }
+}
